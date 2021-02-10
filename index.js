ee9b5d95ebb59ee525a857d2fe562f4eb5b34593
Sahal <sahalbot@outlook.com>
2021-02-10 15:55:04 +0400

    Added Basic Command Handler

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
 const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', () => {
     console.log('PixelBot Is Online And Ready!')
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(config.token)
