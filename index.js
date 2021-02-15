e6a908636d6c4883d846d0f148f6eabb3333fbad
Sahal <sahalbot@outlook.com>
2021-02-15 22:18:30 +0400

    Added A Basic Command Handler. Also Added A ping Command

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+const command = require('./command')
 const config = require('./config.json')
 
 client.on('ready', () => {
     console.log(`Logged In As ${client.user.tag} And I Am Ready!`)
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(config.token)
