c42bd09c2cbfc069544bc7da72dcc8acb5e2a148
Sahal <sahalbot@outlook.com>
2021-02-17 14:20:07 +0400

    Added Automatic Status Change System

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -14,6 +14,19 @@ module.exports = class extends Event {
 			`Loaded ${this.client.commands.size} Commands!`,
 			`Loaded ${this.client.events.size} Events!`
 		].join('\n'));
+
+		const activities = [
+			`Watching Sahal Code Me`,
+			`The World Burn hehe`,
+			`You People Talk`,
+			`Twitter Cancelling People For No Reason`,
+			`Tom & Jerry`,
+			`pixdev.gq`,
+			`${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} People Using 16Bits!`
+		];
+
+		let i = 0;
+		setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: 'WATCHING' }), 15000);
 	}
 
 };
