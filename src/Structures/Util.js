f12c531914edd700424b7cddae8e3dca3b9bd34a
Sahal <sahalbot@outlook.com>
2021-02-18 13:56:36 +0400

    Util.js Has Been Done

---
+++ src/Structures/Util.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
+const Command = require('./Command');
+
+module.exports = class Util {
+
+	constructor(client) {
+		this.client = client;
+	}
+
+	isClass(input) {
+		return typeof input === 'function' &&
+        typeof input.prototype === 'object' &&
+        input.toString().substring(0, 5) === 'class';
+	}
+
+	get Directory() {
+		return `${path.dirname(require.main.filename)}${path.sep}`;
+	}
+
+	async loadCommands() {
+		return glob(`${this.directory}Commands/**/*.js`).then(commands => {
+			for (const commandFile of commands) {
+				delete require.cache[commandFile];
+				const { name } = path.parse(commandFile);
+				const File = require(commandFile);
+				if (!this.isClass(File)) throw new TypeError(`Command ${name} Doesn't Export A Class.`);
+				const command = new File(this.client, name.toLowerCase());
+				if (!(command instanceof Command)) throw new TypeError(`Command ${name} Doesn't Belong In Commands.`);
+				this.client.commands.set(command.name, command);
+				if (command.aliases.length) {
+					for (const alias of command.aliases) {
+						this.client.aliases.set(alias, command.name);
+					}
+				}
+			}
+		});
+	}
+
+};
