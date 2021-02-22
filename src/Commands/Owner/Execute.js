b6419a89bf99641a71e5b5d47fa0826e2a700419
Sahal <sahalbot@outlook.com>
2021-02-22 14:38:53 +0400

    Locl Channel And Unlock Channel Command Is DONE + Fix In Execute Command

--- src/Commands/Owner/Execute.js
+++ src/Commands/Owner/Execute.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(message, ...args) {
+	async run(message, args) {
 		exec(args.join(' '), (error, stdout) => {
 			const response = stdout || error;
 			message.channel.send(response, { split: true, code: true });
