6b8a17cceeabdb3a76cb0323986b9be29f170234
Sahal <sahal@sahaldev.gq>
2021-04-27 10:57:51 +0400

    :sparkles: | Added Logs Event For guildMemberAdd

--- src/Events/guild/guildMemberAdd.js
+++ src/Events/guild/guildMemberAdd.js
@@ -1,13 +1,15 @@
+/* eslint-disable max-len */
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable camelcase */
 const Event = require('../../Structures/Event');
 const config = require('../../../config.json');
 const SixteenEmbed = require('../../Structures/SixteenEmbed');
+const moment = require('moment');
 
 module.exports = class extends Event {
 
 	async run(guildMemberAdd) {
-		const { welcome_role_name, welcome_channel_id } = config;
+		const { welcome_role_name, welcome_channel_id, logs_channel_name } = config;
 
 		const welcomeRole = guildMemberAdd.guild.roles.cache.find(role => role.name === welcome_role_name);
 
@@ -21,6 +23,19 @@ module.exports = class extends Event {
 				`Hey There <@${guildMemberAdd.user.id}>! Welcome To The Server And Make Sure To Read The Rules And Have Fun :D`
 			]);
 		guildMemberAdd.guild.channels.cache.get(welcome_channel_id).send(embed);
+
+		const logembed = new SixteenEmbed()
+		    .setColor('RANDOM')
+			.setAuthor('A New Member Has Joined The Server')
+			.setThumbnail(guildMemberAdd.user.displayAvatarURL({ dynamic: true, size: 512 }))
+			.setDescription([
+				`**- Username:** ${guildMemberAdd.user.username}`,
+				`**- Discriminator:** ${guildMemberAdd.user.discriminator}`,
+				`**- ID:** ${guildMemberAdd.id}`,
+				`**- Time Created:** ${moment(guildMemberAdd.user.createdTimestamp).format('LT')} ${moment(guildMemberAdd.user.createdTimestamp).format('LL')} ${moment(guildMemberAdd.user.createdTimestamp).fromNow()}`
+			]);
+		const channel = guildMemberAdd.guild.channels.cache.find(ch => ch.name === logs_channel_name);
+		if (channel) channel.send(logembed);
 	}
 
 };
