9c730a6b80fddf26b88a3a2a42915103236efb17
Sahal <sahal@sahaldev.gq>
2021-04-27 10:37:50 +0400

    :art: :adhesive_bandage: | Added Embeds To Welcome Message And Removed The Option To A Welcome Message In config.json File.

--- src/Events/guild/guildMemberAdd.js
+++ src/Events/guild/guildMemberAdd.js
@@ -1,16 +1,26 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable camelcase */
 const Event = require('../../Structures/Event');
 const config = require('../../../config.json');
+const SixteenEmbed = require('../../Structures/SixteenEmbed');
 
 module.exports = class extends Event {
 
 	async run(guildMemberAdd) {
-		const { welcome_role_name, welcome_channel_id, welcome_message } = config;
+		const { welcome_role_name, welcome_channel_id } = config;
 
 		const welcomeRole = guildMemberAdd.guild.roles.cache.find(role => role.name === welcome_role_name);
 
 		guildMemberAdd.roles.add(welcomeRole);
-		guildMemberAdd.guild.channels.cache.get(welcome_channel_id).send(welcome_message);
+
+		const embed = new SixteenEmbed()
+		    .setColor('RANDOM')
+			.setAuthor(guildMemberAdd.guild.name, this.client.user.displayAvatarURL({ dynamic: true }))
+			.setTitle('Welcome To The Server!')
+			.setDescription([
+				`Hey There <@${guildMemberAdd.user.id}>! Welcome To The Server And Make Sure To Read The Rules And Have Fun :D`
+			]);
+		guildMemberAdd.guild.channels.cache.get(welcome_channel_id).send(embed);
 	}
 
 };
